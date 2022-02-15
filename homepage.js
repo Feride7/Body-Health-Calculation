045b7e6f1724ce87880aa9ed59d5a4a0097e8996
Ömer Koçar <kocaromer@gmail.com>
2022-02-15 22:59:51 +0300

    add and fix responsive navbar

--- homepage.js
+++ homepage.js
@@ -1,3 +1,6 @@
+const hamburger = document.getElementById("hamburger");
+const navUL = document.querySelector(".nav-top-ul");
 
-
-
+hamburger.addEventListener("click", () => {
+    navUL.classList.toggle("show")
+})
